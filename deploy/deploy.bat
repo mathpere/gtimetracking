7d2c2dfd37c6ab4c2531146952a89fbf85d5ddeb
mathieu.perez <mathieu.perez@55644f14-5342-3197-6aeb-5c73db8d3189>
2010-06-20 13:27:19 +0000

    maven project which aims to deploy libraries on google code

---
+++ deploy/deploy.bat
@@ -0,0 +1,11 @@
+call mvn deploy:deploy-file -DgroupId=com.google.gdata -DartifactId=gdata-calendar -Dversion=2.0 -Dpackaging=jar -Dfile=gdata-calendar-2.0.jar -Durl=svn:https://gtimetracking.googlecode.com/svn/repo -DrepositoryId=googlecode 
+
+call mvn deploy:deploy-file -DgroupId=com.google.gdata -DartifactId=gdata-calendar-meta -Dversion=2.0 -Dpackaging=jar -Dfile=gdata-calendar-meta-2.0.jar  -Durl=svn:https://gtimetracking.googlecode.com/svn/repo -DrepositoryId=googlecode 
+
+call mvn deploy:deploy-file -DgroupId=com.google.gdata -DartifactId=gdata-client -Dversion=1.0 -Dpackaging=jar -Dfile=gdata-client-1.0.jar  -Durl=svn:https://gtimetracking.googlecode.com/svn/repo -DrepositoryId=googlecode 
+
+call mvn deploy:deploy-file -DgroupId=com.google.gdata -DartifactId=gdata-client-meta -Dversion=1.0 -Dpackaging=jar -Dfile=gdata-client-meta-1.0.jar  -Durl=svn:https://gtimetracking.googlecode.com/svn/repo -DrepositoryId=googlecode 
+
+call mvn deploy:deploy-file -DgroupId=com.google.gdata -DartifactId=gdata-core -Dversion=1.0 -Dpackaging=jar -Dfile=gdata-core-1.0.jar  -Durl=svn:https://gtimetracking.googlecode.com/svn/repo -DrepositoryId=googlecode 
+
+call mvn deploy:deploy-file -DgroupId=net.sourceforge.jexcelapi -DartifactId=jxl -Dversion=2.6.12 -Dpackaging=jar -Dfile=jxl-2.6.12.jar  -Durl=svn:https://gtimetracking.googlecode.com/svn/repo -DrepositoryId=googlecode 
