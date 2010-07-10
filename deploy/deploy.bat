600a5d1a851ad8375e3fa5d7b5d2e939905dae21
mathieu.perez <mathieu.perez@55644f14-5342-3197-6aeb-5c73db8d3189>
2010-07-10 17:56:17 +0000

    adds gdocs lib

--- deploy/deploy.bat
+++ deploy/deploy.bat
@@ -9,3 +9,9 @@ call mvn deploy:deploy-file -DgroupId=com.google.gdata -DartifactId=gdata-client
 call mvn deploy:deploy-file -DgroupId=com.google.gdata -DartifactId=gdata-core -Dversion=1.0 -Dpackaging=jar -Dfile=gdata-core-1.0.jar  -Durl=svn:https://gtimetracking.googlecode.com/svn/repo -DrepositoryId=googlecode 
 
 call mvn deploy:deploy-file -DgroupId=net.sourceforge.jexcelapi -DartifactId=jxl -Dversion=2.6.12 -Dpackaging=jar -Dfile=jxl-2.6.12.jar  -Durl=svn:https://gtimetracking.googlecode.com/svn/repo -DrepositoryId=googlecode 
+
+call mvn deploy:deploy-file -DgroupId=com.google.gdata -DartifactId=gdata-docs-meta -Dversion=3.0 -Dpackaging=jar -Dfile=gdata-docs-meta-3.0.jar  -Durl=svn:https://gtimetracking.googlecode.com/svn/repo -DrepositoryId=googlecode 
+
+call mvn deploy:deploy-file -DgroupId=com.google.gdata -DartifactId=gdata-docs -Dversion=3.0 -Dpackaging=jar -Dfile=gdata-docs-3.0.jar  -Durl=svn:https://gtimetracking.googlecode.com/svn/repo -DrepositoryId=googlecode 
+
+call mvn deploy:deploy-file -DgroupId=com.google.gdata -DartifactId=gdata-media -Dversion=1.0 -Dpackaging=jar -Dfile=gdata-media-1.0.jar  -Durl=svn:https://gtimetracking.googlecode.com/svn/repo -DrepositoryId=googlecode 
